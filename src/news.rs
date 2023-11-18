11ba0f3a9373c957979555153b657a0e7739985c
Hai Nguyen Hoang <hai.nguyenhoang@proton.me>
2023-11-18 18:00:34 +0100

    add get_news

--- src/news.rs
+++ src/news.rs
@@ -15,6 +15,14 @@ impl News {
         }
     }
 
+    pub fn get_news(&self) -> Vec<&NewsItem> {
+        let mut result: Vec<&NewsItem> = vec![];
+        for item in &self.news {
+            result.push(item);
+        }
+        result
+    }
+
     pub fn get_different_items(&self, old_news: &Self, check_count: u64, platforms: &BTreeSet<String>) -> Vec<&NewsItem> {
         let mut result = vec![];
         for item in &self.news[..check_count as usize] {
